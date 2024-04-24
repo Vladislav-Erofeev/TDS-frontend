408e91b075e9c7663accc4e3062abf47b88075ad
Vladislav <20031911@mail.ru>
2024-04-24 18:05:47 +0300

    feat: у гостя больше нет возможности добавлять объекты

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -3,15 +3,16 @@ import MapComponent from "../components/MapComponent";
 import SearchComponent from "../components/SearchComponent";
 import ObjectInfoComponent from "../components/ObjectInfoComponent";
 import MapObjectsComponent from "../components/MapObjectsComponent";
+import {hasRole} from "../data/functions";
 
 const MapPage = () => {
     const [map, setMap] = useState(null)
     return (
         <>
-            <MapComponent setMap={setMap} />
-            <MapObjectsComponent map={map} />
-            <SearchComponent map={map} />
-            <ObjectInfoComponent map={map} />
+            <MapComponent setMap={setMap}/>
+            <MapObjectsComponent map={map}/>
+            <SearchComponent map={map}/>
+            {hasRole('USER', 'ADMIN') ? <ObjectInfoComponent map={map}/> : null}
         </>
     );
 };
